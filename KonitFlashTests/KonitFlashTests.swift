1f74a8ab04a46bcb5980d608c1f429bdd2bcf8c7
Konit611 <konit611@gmail.com>
2026-02-01 11:38:34 +0900

    Initial Commit

---
+++ KonitFlashTests/KonitFlashTests.swift
@@ -0,0 +1,17 @@
+//
+//  KonitFlashTests.swift
+//  KonitFlashTests
+//
+//  Created by GEUNIL on 2026/02/01.
+//
+
+import Testing
+@testable import KonitFlash
+
+struct KonitFlashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
